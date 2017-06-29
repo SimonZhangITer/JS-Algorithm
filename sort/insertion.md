9d549c5768a373bab1167de7b4d94e201aa1adcf
张韧 <simonzhangr@foxmail.com>
2017-06-29 16:00:00 +0800

    insert es6

--- sort/insertion.md
+++ sort/insertion.md
@@ -19,15 +19,15 @@
 
 ```javascript
 function insertSort(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    var currentData = arr[i];
-    var preIndex = i - 1; // 从当前元素的前一个开始比较
-    while (preIndex >= 0 && arr[preIndex] > currentData) {
-      arr[preIndex + 1] = arr[preIndex]; // 互换位置，但是currentData已经存储，所以不用考虑arr[preIndex]的值
-      preIndex--;
+    for (let i = 1; i < arr.length; i++) {
+        let currentData = arr[i],
+            preIndex = i - 1; // 从当前元素的前一`个开始比较
+        while (preIndex >= 0 && arr[preIndex] > currentData) {
+            arr[preIndex + 1] = arr[preIndex]; // 互换位置，但是currentData已经存储，所以不用考虑arr[preIndex]的值
+            preIndex--;
+        }
+        arr[preIndex + 1] = currentData;
     }
-    arr[preIndex + 1] = currentData;
-  }
-  return arr
+    return arr
 }
 ```
--- sort/testDemo.js
+++ sort/testDemo.js
@@ -5,38 +5,44 @@
 // 5. 以此类推，直到所有元素排序完毕
 
 var arr = [
-  2,
-  1,
-  9,
-  6,
-  4,
-  3
+    2,
+    1,
+    9,
+    6,
+    4,
+    3
 ]
 
 // 每找出一个最小元素，将此元素添加进临时数组, 并在原数组中删除此元素，继续遍历，直到原数组的长度为0
 function selectionSort(arr) {
-  let temp = [], // 存放最小的元素，每次push进去
-    len = arr.length
-  while (temp.length < len) {
-    let min = Math.min(...arr) // 当前数组的最小元素
-    temp.push(min)
-    arr.splice(arr.indexOf(min), 1) // 移除最小元素
-  }
-  return temp
+    let temp = [], // 存放最小的元素，每次push进去
+        len = arr.length
+    while (temp.length < len) {
+        let min = Math.min(...arr) // 当前数组的最小元素
+        temp.push(min)
+        arr.splice(arr.indexOf(min), 1) // 移除最小元素
+    }
+    return temp
 }
 
 // 插入排序
 function insertSort(arr) {
-  for (var i = 1; i < arr.length; i++) {
-    var currentData = arr[i];
-    var preIndex = i - 1; // 从当前元素的前一个开始比较
-    while (preIndex >= 0 && arr[preIndex] > currentData) {
-      arr[preIndex + 1] = arr[preIndex]; // 互换位置，但是currentData已经存储，所以不用考虑arr[preIndex]的值
-      preIndex--;
+    for (let i = 1; i < arr.length; i++) {
+        let currentData = arr[i],
+            preIndex = i - 1; // 从当前元素的前一`个开始比较
+        while (preIndex >= 0 && arr[preIndex] > currentData) {
+            arr[preIndex + 1] = arr[preIndex]; // 互换位置，但是currentData已经存储，所以不用考虑arr[preIndex]的值
+            preIndex--;
+        }
+        arr[preIndex + 1] = currentData;
     }
-    arr[preIndex + 1] = currentData;
-  }
-  return arr
+    return arr
 }
 
-console.log(insertSort(arr));
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+}
+
+console.log(bubbleSort(arr));
